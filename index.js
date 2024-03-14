6296342ea22cb43c91134e210a4d8ba0dc275aef
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-03-14 21:56:06 +0200

    Added JS functions

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL: "https://shoppingapp-ead42-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingCartDB = ref(database, "itemCart")
+
+const shoppingCartEL = document.getElementById("shopping-cart")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
-})
+
+    addButtonEl.addEventListener("click", function () {
+        let inputValue = inputFieldEl.value
+        push(shoppingCartDB, inputValue)
+        clearInputFieldEL()
+
+        console.log(inputValue)
+    })
+
+    onValue (shoppingCartDB, function(snapshot) {
+        let cartItemsArray= Object.values(snapshot.val())
+        clearShopingCartEL()
+ 
+        for (let i = 0; i < cartItemsArray.length; i++ ) {
+         addToCartEL(cartItemsArray[i])
+        }
+        
+     })
+
+    function clearShopingCartEL(){
+    shoppingCartEL.innerHTML = ""
+    }
+
+    function addToCartEL(itemValue) {
+        shoppingCartEL.innerHTML += `<li>${itemValue}</li>`
+    }
+
+    function clearInputFieldEL() {
+        inputFieldEl.value = ""
+    }
+
+   
+
